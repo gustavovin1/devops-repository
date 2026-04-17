fc2d9827d4c3a5d87e1a5387e8037b06b22446b7
GUSTA <gustavinicristo@gmail.com>
2026-04-17 11:29:18 -0300

    alteracao do endpoint para retornar um inteiro aleatorio

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-
+import random
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 @app.get("/teste1")
-async def funcaoTeste():
-    return {"teste": "deu certo!"}
+async def funcaoteste():
+    return {"teste": True, "random_int": random.randint(1, 10)}
