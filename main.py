e0ecef28075f942991d625be35c3243edee497c3
GUSTA <gustavinicristo@gmail.com>
2026-04-17 11:31:21 -0300

    alteracao do nome das rotas

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from fastapi import FastAPI
 import random
 app = FastAPI()
 
-@app.get("/")
+@app.get("/hello")
 async def root():
     return {"message": "Hello World"}
-@app.get("/teste1")
+@app.get("/random-int")
 async def funcaoteste():
     return {"teste": True, "random_int": random.randint(1, 10)}
