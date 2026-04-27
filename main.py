17b3de969ce161337821468f1f2cca35059227f2
Gusta <gustavinicristo@gmail.com>
2026-04-27 15:45:05 -0300

    add testes

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-from fastapi import FastAPI
 import random
+from fastapi import FastAPI
+
 app = FastAPI()
+
+
+def get_hello_message():
+    return {"message": "Hello World"}
+
+
+def generate_random_number():
+    return random.randint(1, 10)
+
 
 @app.get("/hello")
 async def root():
-    return {"message": "Hello World"}
+    return get_hello_message()
+
+
 @app.get("/random-int")
 async def random_int():
-    return {"teste": True, "random_int": random.randint(1, 10)}
+    return {"teste": True, "random_int": generate_random_number()}
